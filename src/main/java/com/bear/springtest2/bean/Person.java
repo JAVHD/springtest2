636e0ec13597fbbfc3dd2cb84096b9efaa7f886a
bear <593788753@qq.com>
2020-07-11 13:46:31 +0800

    first commit

---
+++ src/main/java/com/bear/springtest2/bean/Person.java
@@ -0,0 +1,42 @@
+package com.bear.springtest2.bean;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: bear
+ * Date: 2020/7/11 上午11:50
+ * Description:
+ */
+
+public class Person {
+
+    public Person() {
+        System.out.println("bean 注册");
+    }
+
+    private String name;
+    private Integer age;
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+            "name='" + name + '\'' +
+            ", age=" + age +
+            '}';
+    }
+}
---
+++ src/main/java/com/bear/springtest2/demo/BeanDemo.java
@@ -0,0 +1,34 @@
+package com.bear.springtest2.demo;
+
+import com.bear.springtest2.bean.Person;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: bear
+ * Date: 2020/7/11 上午11:55
+ * Description:
+ */
+
+public class BeanDemo {
+
+    public static void main(String[] args) {
+
+        //加载配置文件
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        //applicationContext.get
+
+        //获取bean对象
+        Person person = (Person) applicationContext.getBean("person"); //根据bean id 获取
+
+        person.setAge(18);
+        person.setName("bear");
+
+        System.out.println(person.toString());
+
+
+
+    }
+}
