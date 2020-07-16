e8caeaf2e2b66e068801863d48b32bfd8f94c7a4
bear <593788753@qq.com>
2020-07-16 17:07:33 +0800

    xml import resource

---
+++ src/main/java/com/bear/springtest2/demo/ActiveMQConsumerDemo.java
@@ -0,0 +1,17 @@
+package com.bear.springtest2.demo;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: bear
+ * Date: 2020/7/14 下午2:06
+ * Description: 消费消息
+ */
+
+public class ActiveMQConsumerDemo {
+    public static void main(String[] args) { //ClassPathXmlApplicationContext
+
+    }
+}
